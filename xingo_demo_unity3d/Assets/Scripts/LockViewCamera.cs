b6be6dcae80684c449b2d9ea9a46d5d3185dc5b0
SimonIdf <sametoall@163.com>
2017-05-26 17:06:22 +0800

    123
    123

---
+++ xingo_demo_unity3d/Assets/Scripts/LockViewCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockViewCamera : MonoBehaviour {
+
+    public Transform follow;
+
+    private Vector3 originalPos;
+
+    private Vector3 followOriginalPos;
+
+    private Vector3 originalDisVector;
+
+    public float zoom;
+
+    void Awake()
+    {
+        followOriginalPos = follow.position;
+        originalPos = transform.position;
+
+        originalDisVector = originalPos - followOriginalPos;
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.mouseScrollDelta.y!=0)
+        {
+            if(Input.mouseScrollDelta.y>0)
+            {
+                zoom -= Time.deltaTime*3;
+            }
+            else
+            {
+                zoom += Time.deltaTime*3;
+            }
+        }
+
+        if (zoom >= 1) zoom = 1;
+        if (zoom <= 0.5f) zoom = 0.5f;
+
+        transform.position = followOriginalPos + originalDisVector*zoom + follow.position - followOriginalPos;
+	}
+}
---
+++ xingo_demo_unity3d/Assets/Test/TimeScale.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScale : MonoBehaviour
+{
+
+    public float timeScale;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Time.timeScale = timeScale;
+	}
+}
